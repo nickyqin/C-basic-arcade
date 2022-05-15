d2dbc5410cf42c1778c6a61ebeab1fc78cfc9902
Nicky Qin <nqinnqin23@gmail.com>
2022-05-15 17:48:38 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Nicky Qin on 5/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
