2482fa877b689ee0d4013f59761aef2fd1985287
Nicky Qin <nqinnqin23@gmail.com>
2022-05-15 18:28:02 -0400

    "Add Triangle Tester app"

--- test/main.cpp
+++ test/main.cpp
@@ -7,8 +7,29 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int ang1, ang2, ang3;
+    
+    cout << "Triangle Tester\n\n" << "Triangle angle one: " << endl;
+    cin >> ang1;
+    
+    cout << "Angle two: " << endl;
+    cin >> ang2;
+    
+    cout << "Angle three: " << endl;
+    cin >> ang3;
+    
+    if (ang1 + ang2 + ang3 == 180)
+    {
+        cout << "Valid Triangle!" << endl;
+    }
+    else
+    {
+        cout << "Invalid Triangle!" << endl;
+    }
+    
     return 0;
 }
