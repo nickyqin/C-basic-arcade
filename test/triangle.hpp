4cc6fc96746a65fb10d807e1637019d2c7ade06f
Nicky Qin <nqinnqin23@gmail.com>
2022-05-15 22:44:15 -0400

    Add separate file and header for triangle

---
+++ test/triangle.hpp
@@ -0,0 +1,18 @@
+//
+//  triangle.hpp
+//  test
+//
+//  Created by Nicky Qin on 5/15/22.
+//
+
+#ifndef triangle_hpp
+#define triangle_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int checkTriangle();
+
+#endif /* triangle_hpp */
